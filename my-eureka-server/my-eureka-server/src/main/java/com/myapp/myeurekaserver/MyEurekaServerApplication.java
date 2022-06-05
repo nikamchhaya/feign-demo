68a1fc2d22ce00c68fc199bc445796b749d65400
nikamchhaya <cloudcomputingchh@gmail.com>
2022-06-05 20:05:43 +0530

    first commit

---
+++ my-eureka-server/my-eureka-server/src/main/java/com/myapp/myeurekaserver/MyEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.myeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyEurekaServerApplication.class, args);
+	}
+
+}
---
+++ myeureka/src/test/java/com/example/myeureka/MyeurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyeurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
